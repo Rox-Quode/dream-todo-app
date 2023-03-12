16997356dbbe9f8a726e4f14187809371208b2b3
Rox-Quode <92692755+Rox-Quode@users.noreply.github.com>
2023-03-12 18:16:21 +0200

    update additem

--- components/AddItem.jsx
+++ components/AddItem.jsx
@@ -46,6 +46,7 @@ export function AddItem({ refreshData }) {
             name="dueDate"
             value={dueDate}
             min={format(new Date(), "dd/MM/yyyy")}
+            placeholder="pick date here📅"
             className="w-full rounded-md border-gray-200 py-2.5 xs:pr-2 sm:pr-6 sm:text-sm"
             onChange={(e) => setDueDate(e.target.value)}
           ></input>
